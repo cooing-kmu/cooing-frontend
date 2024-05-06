85843b0d103d85253343207d4212a32dac4abc60
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-06 22:02:56 +0900

    design: 마이페이지 내 하위 페이지 디자인 파일 (배경색 지정 외에 된 것 없음)

---
+++ src/pages/profile/Styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  height: 100vh;
+`
+
+export const BackgroundContainer = styled.div`
+  display: flex;
+  width: 428px;
+  height: 250px;
+  padding: 25px 31px;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 18px;
+  flex-shrink: 0;
+  border-radius: 40px 40px 0px 0px;
+  background: linear-gradient(180deg, #fd814a 0%, #fc5c4c 100%);
+`
