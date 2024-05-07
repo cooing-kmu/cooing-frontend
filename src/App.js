7ae27299535eaa6381e4e11840ce981fe5966e6e
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-08 07:28:58 +0900

    feat: 페이지 라우트 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import './App.css'
-import { ThemeProvider } from 'styled-components'; // 1. Global color를 사용하기 위한 import
-import React, { Component } from 'react';
-import { RecoilRoot } from 'recoil';
-import { CookiesProvider } from 'react-cookie';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
+import { ThemeProvider } from 'styled-components' // 1. Global color를 사용하기 위한 import
+import React, { Component } from 'react'
+import { RecoilRoot } from 'recoil'
+import { CookiesProvider } from 'react-cookie'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Announcement from './pages/announcement/Announcement'
+import Community from './pages/community/Community'
+import MainPage from './pages/mainpage/MainPage'
+import Chatting from './pages/chatting/Chatting'
+import MyPage from './pages/mypage/Mypage'
 
 function App() {
   return (
     // <ThemeProvider>
-      <RecoilRoot>
-        <div id="App">
-          <CookiesProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/sign-up" element={<SignUp/>}></Route>
-                <Route path="/sign-in" element={<SignIn/>}></Route>
-              </Routes>
-            </BrowserRouter>
-          </CookiesProvider>
-        </div>
-      </RecoilRoot>
+    <RecoilRoot>
+      <div id='App'>
+        <CookiesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<MainPage />}></Route>
+              <Route path='/sign-up' element={<SignUp />}></Route>
+              <Route path='/sign-in' element={<SignIn />}></Route>
+              <Route path='/announcement' element={<Announcement />}></Route>
+              <Route path='/community' element={<Community />}></Route>
+              <Route path='/main-page' element={<MainPage />}></Route>
+              <Route path='/chatting' element={<Chatting />}></Route>
+              <Route path='/my-page' element={<MyPage />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </CookiesProvider>
+      </div>
+    </RecoilRoot>
     // </ThemeProvider>
   )
 }
