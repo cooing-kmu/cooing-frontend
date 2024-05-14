d993ebdb73b36097d1830a4c79747d47cc876bde
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-14 17:58:52 +0900

    feat: 자립 체크리스트 헤더 추가

--- src/pages/checklist/CheckList.js
+++ src/pages/checklist/CheckList.js
@@ -1,5 +1,10 @@
 import * as style from './Styles'
+import Header from '../../components/header/Header'
 
 export default function CheckList() {
-  return <style.MainContainer>자립 체크리스트 테스트</style.MainContainer>
+  return (
+    <style.MainContainer>
+      <Header title='자립 체크리스트' />
+    </style.MainContainer>
+  )
 }
