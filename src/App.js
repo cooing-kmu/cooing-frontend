a2aece945e9788c03dc98cfb7ac3a6ba1ea4fc54
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-14 19:36:03 +0900

    cooing-frontend-9 Cherry-pick

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css'
 import { ThemeProvider } from 'styled-components' // 1. Global color를 사용하기 위한 import
-import React, { Component } from 'react'
 import { RecoilRoot } from 'recoil'
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import MyPage from './pages/mypage/MyPage'
+import Profile from './pages/profile/Profile'
+import CheckList from './pages/checklist/CheckList'
+import InterestInfo from './pages/interestinfo/InterestInfo'
 import Announcement from './pages/announcement/Announcement'
 import Community from './pages/community/Community'
-import MainPage from './pages/mainpage/MainPage'
 import Chatting from './pages/chatting/Chatting'
-import MyPage from './pages/mypage/Mypage'
+import MainPage from './pages/mainpage/MainPage'
+import ThinkInfo from './pages/thinkinfo/ThinkInfo'
+import InterestEdit from './pages/interestedit/InterestEdit'
+import ThinkEdit from './pages/thinkedit/ThinkEdit'
 
 function App() {
   return (
@@ -20,14 +25,20 @@ function App() {
         <CookiesProvider>
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<MainPage />}></Route>
+              <Route path='/' element={<MyPage />}></Route>
+              <Route path='/my-page' element={<MyPage />}></Route>
+              <Route path='/profile' element={<Profile />}></Route>
+              <Route path='/interest-info' element={<InterestInfo />}></Route>
+              <Route path='/think-info' element={<ThinkInfo />}></Route>
+              <Route path='/check-list' element={<CheckList />}></Route>
               <Route path='/sign-up' element={<SignUp />}></Route>
               <Route path='/sign-in' element={<SignIn />}></Route>
               <Route path='/announcement' element={<Announcement />}></Route>
               <Route path='/community' element={<Community />}></Route>
               <Route path='/main-page' element={<MainPage />}></Route>
               <Route path='/chatting' element={<Chatting />}></Route>
-              <Route path='/my-page' element={<MyPage />}></Route>
+              <Route path='/interest-edit' element={<InterestEdit />}></Route>
+              <Route path='/think-edit' element={<ThinkEdit />}></Route>
             </Routes>
           </BrowserRouter>
         </CookiesProvider>
