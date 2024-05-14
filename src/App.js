ce52f407ac6e848cbebc490554433e9b04a4b24d
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-14 19:36:03 +0900

    feat: 매칭 정보 (관심/ 관심) , 매칭 정보 변경 (고민/ 관심) 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import SignIn from './pages/SignIn'
 import MyPage from './pages/mypage/MyPage'
 import Profile from './pages/profile/Profile'
 import CheckList from './pages/checklist/CheckList'
-import MatchingInfo from './pages/matchinginfo/MatchingInfo'
+import InterestInfo from './pages/interestinfo/InterestInfo'
 import Announcement from './pages/announcement/Announcement'
 import Community from './pages/community/Community'
 import Chatting from './pages/chatting/Chatting'
 import MainPage from './pages/mainpage/MainPage'
+import ThinkInfo from './pages/thinkinfo/ThinkInfo'
+import InterestEdit from './pages/interestedit/InterestEdit'
+import ThinkEdit from './pages/thinkedit/ThinkEdit'
 
 function App() {
   return (
@@ -25,7 +28,8 @@ function App() {
               <Route path='/' element={<MyPage />}></Route>
               <Route path='/my-page' element={<MyPage />}></Route>
               <Route path='/profile' element={<Profile />}></Route>
-              <Route path='/matching-info' element={<MatchingInfo />}></Route>
+              <Route path='/interest-info' element={<InterestInfo />}></Route>
+              <Route path='/think-info' element={<ThinkInfo />}></Route>
               <Route path='/check-list' element={<CheckList />}></Route>
               <Route path='/sign-up' element={<SignUp />}></Route>
               <Route path='/sign-in' element={<SignIn />}></Route>
@@ -33,6 +37,8 @@ function App() {
               <Route path='/community' element={<Community />}></Route>
               <Route path='/main-page' element={<MainPage />}></Route>
               <Route path='/chatting' element={<Chatting />}></Route>
+              <Route path='/interest-edit' element={<InterestEdit />}></Route>
+              <Route path='/think-edit' element={<ThinkEdit />}></Route>
             </Routes>
           </BrowserRouter>
         </CookiesProvider>
