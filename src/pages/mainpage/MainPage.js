e6f0765860cd67b705cf77b1da0b4a0a6a009ffc
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-16 23:09:12 +0900

    feat: 메인 페이지 UI 구현

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,21 +1,31 @@
 import Footer from '../../components/footer/Footer'
 import React from 'react'
-import styled from 'styled-components'
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #fffad0;
-  height: 100vh;
-  gap: 360px;
-`
+import * as style from './Styles'
+import { useNavigate } from 'react-router-dom'
+import { ReactComponent as CooingLogo } from '../../assets/cooingLogo.svg'
+import { ReactComponent as Ic_Alarm } from '../../assets/icons/icon-alarm.svg'
+import { ReactComponent as Ic_People } from '../../assets/icons/icon-people.svg'
+import Image_EmptyBackGround from '../../assets/images/image-empty-background.png'
 
 export default function MainPage() {
+  const navigate = useNavigate()
+
   return (
-    <MainContainer>
+    <style.MainContainer>
       메인 페이지 네비게이션 테스트
+      <style.HeaderContainer>
+        <style.ButtonContainer>
+          <Ic_Alarm onClick={() => navigate('/alarm')} />
+        </style.ButtonContainer>
+        <style.CooingLogo>
+          <CooingLogo />
+        </style.CooingLogo>
+        <style.ButtonContainer>
+          <Ic_People onClick={() => navigate('/mate-info')} />
+        </style.ButtonContainer>
+      </style.HeaderContainer>
+      {/*<img src={Image_EmptyBackGround} alt='빈 과자집' />*/}
       <Footer />
-    </MainContainer>
+    </style.MainContainer>
   )
 }
