9b6450f65244c11c1f3526d76b2f3d2b607297b2
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-16 23:09:33 +0900

    feat&design: 메이트 정보 페이지 기본틀만

---
+++ src/pages/mateinfo/MateInfo.js
@@ -0,0 +1,13 @@
+import * as style from './Styles'
+import Header from '../../components/header/Header'
+
+export default function MateInfo() {
+  return (
+    <style.MainContainer>
+      <style.BackgroundContainer>
+        <Header title='MATE 정보' />
+        메이트 정보 페이지 테스트
+      </style.BackgroundContainer>
+    </style.MainContainer>
+  )
+}
---
+++ src/pages/mateinfo/Styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  height: 100vh;
+`
+
+export const BackgroundContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 250px;
+  flex-direction: column;
+  align-items: center;
+  gap: 18px;
+  flex-shrink: 0;
+  background: linear-gradient(180deg, #fd814a 0%, #fc5c4c 100%);
+`
