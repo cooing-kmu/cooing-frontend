ab80be0727f6601dbefbe072ef74829738ad2018
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-16 21:17:47 +0900

    design: 자립 체크리스트 UI 디자인 구현

--- src/pages/checklist/Styles.js
+++ src/pages/checklist/Styles.js
@@ -7,3 +7,34 @@ export const MainContainer = styled.div`
   background-color: #fffad0;
   height: 100vh;
 `
+
+export const MenuContainer = styled.div`
+  display: flex;
+  width: 376px;
+  //height: 271px;
+  border-radius: 40px;
+  background: #fff;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  flex-direction: column;
+  align-items: stretch;
+  margin-top: 100px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`
+
+export const ItemContainer = styled.div`
+  display: flex;
+  height: 56px;
+  align-items: center;
+  font-size: 16px;
+  margin-left: 28px;
+  margin-right: 28px;
+  gap: 6px;
+  justify-content: space-between;
+  cursor: pointer;
+  border-bottom: ${(props) => (props.clicked ? 'none' : 'solid 1px #a6a6a6')};
+  &:last-child {
+    border-bottom: none;
+  }
+  color: ${(props) => (props.clicked ? '#fd814a' : 'black')};
+`
