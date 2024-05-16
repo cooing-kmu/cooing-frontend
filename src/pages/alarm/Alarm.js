906cdea49051054c626be06501331a6eff7bda71
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-16 23:09:46 +0900

    feat&design: 알람 페이지 기본틀만

---
+++ src/pages/alarm/Alarm.js
@@ -0,0 +1,11 @@
+import * as style from './Styles'
+import Header from '../../components/header/Header'
+
+export default function Alarm() {
+  return (
+    <style.MainContainer>
+      <Header title={'알림'} />
+      알림 페이지 테스트
+    </style.MainContainer>
+  )
+}
