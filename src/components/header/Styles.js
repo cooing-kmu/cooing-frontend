77ef8ce1a4f26523289d38917be5df0dd556d447
suwith <suwith@kookmin.ac.kr>
2024-05-16 23:30:29 +0900

    merge: develop 브랜치에서 header 병합

---
+++ src/components/header/Styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  position: sticky;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-weight: bold;
+  font-size: 16px;
+  text-align: center;
+  margin-top: 30px;
+  color: ${(props) => (props.IsBlack ? 'black' : 'white')};
+  &:first-child {
+    float: left;
+    position: absolute;
+  }
+  &:last-child {
+    float: right;
+    position: absolute;
+  }
+`
+
+export const ButtonContainer = styled.div`
+  cursor: pointer;
+`
