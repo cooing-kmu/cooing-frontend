fcab49c437811540ace277e42c51b46a65baac53
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-16 23:09:00 +0900

    design: 메인 페이지 UI 디자인 구현

---
+++ src/pages/mainpage/Styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fffad0;
+  height: 100vh;
+  gap: 360px;
+`
+
+export const CooingLogo = styled.div`
+  width: 150px;
+  height: 73px;
+`
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 60px;
+  align-items: center;
+`
+
+export const ButtonContainer = styled.div`
+  cursor: pointer;
+`
