3bfa088f61ccda266d9ffdefb8b4621df8c4507a
chanwoo <chanwoo0607@naver.com>
2024-05-21 02:48:50 +0900

    feat: App.js 추가

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import React, { Component } from 'react'
 import { RecoilRoot } from 'recoil'
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import SignUp2 from './pages/SignUp2'
-import SignUp3 from './pages/SignUp3'
-import SignUp4 from './pages/SignUp4'
-import SignUp5 from './pages/SignUp5'
-import Community from './pages/Community'
-import Club from './pages/Club'
-import Comment from './pages/Comment'
-import FreeBoard from './pages/FreeBoard'
-import MyWrote from './pages/MyWrote'
-import Scrap from './pages/Scrap'
-import Study from './pages/Study'
-import Volunteer from './pages/Volunteer'
-import FreeBoardWrite from './pages/FreeBoardWrite'
-import ClubWrite from './pages/ClubWrite'
+import SignUp from './pages/SignUp/SignUp'
+import SignIn from './pages/SignIn/SignIn'
+import SignUp2 from './pages/SignUp/SignUp2'
+import SignUp3 from './pages/SignUp/SignUp3'
+import SignUp4 from './pages/SignUp/SignUp4'
+import SignUp5 from './pages/SignUp/SignUp5'
+import Community from './pages/Community/Community'
+import Club from './pages/Community/Club/Club'
+import Comment from './pages/Community/Comment'
+import FreeBoard from './pages/Community/FreeBoard/FreeBoard'
+import MyWrote from './pages/Community/MyWrote'
+import Scrap from './pages/Community/Scrap'
+import Study from './pages/Community/Study/Study'
+import Volunteer from './pages/Community/Volunteer/Volunteer'
+import FreeBoardWrite from './pages/Community/FreeBoard/FreeBoardWrite'
+import ClubWrite from './pages/Community/Club/ClubWrite'
+import StudyWrite from './pages/Community/Study/StudyWrite'
+import VolunteerWrite from './pages/Community/Volunteer/VolunteerWrite'
+import FreeBoardPost from './pages/Community/FreeBoard/FreeBoardPost'
 
 function App() {
   return (
@@ -43,11 +46,11 @@ function App() {
               <Route path='/scrap' element={<Scrap />}></Route>
               <Route path='/study' element={<Study />}></Route>
               <Route path='/volunteer' element={<Volunteer />}></Route>
-              <Route
-                path='/free-board-write'
-                element={<FreeBoardWrite />}
-              ></Route>
+              <Route path='/free-board-write' element={<FreeBoardWrite />}></Route>
               <Route path='/club-write' element={<ClubWrite />}></Route>
+              <Route path='/study-write' element={<StudyWrite />}></Route>
+              <Route path='/volunteer-write' element={<VolunteerWrite />}></Route>
+              <Route path='/free-board-post' element={<FreeBoardPost />}></Route>              <Route path='/free-board-post' element={<FreeBoardPost />}></Route>
             </Routes>
           </BrowserRouter>
         </CookiesProvider>
