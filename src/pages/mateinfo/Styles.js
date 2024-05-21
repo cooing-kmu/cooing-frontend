30f3b2e788ea9d4b41b6321f725060a8a030519c
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-22 01:46:59 +0900

    design: 매칭 정보 페이지 디자인 구현

--- src/pages/mateinfo/Styles.js
+++ src/pages/mateinfo/Styles.js
@@ -18,3 +18,38 @@ export const BackgroundContainer = styled.div`
   flex-shrink: 0;
   background: linear-gradient(180deg, #fd814a 0%, #fc5c4c 100%);
 `
+
+export const CircleContainer = styled.div`
+  position: absolute;
+  width: 140px;
+  height: 140px;
+  border-radius: 100px;
+  margin-top: 175px;
+  background-color: #fff;
+`
+
+export const ImageContainer = styled.div`
+  position: relative;
+  margin-top: 180px;
+  width: 130px;
+  height: 130px;
+`
+
+export const TextContainer = styled.div`
+  display: flex;
+  margin-top: 90px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  align-items: flex-end;
+  gap: 6px;
+  span:first-child {
+    color: black;
+    font-size: 22px;
+    font-weight: bold;
+  }
+  span:last-child {
+    color: #fd814a;
+    font-size: 14px;
+    font-weight: bold;
+  }
+`
