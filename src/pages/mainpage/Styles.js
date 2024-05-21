87346a27aefee9d3395a89014a06bff89d99e9a5
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-22 01:46:08 +0900

    design: 메인 페이지 디자인 구현

--- src/pages/mainpage/Styles.js
+++ src/pages/mainpage/Styles.js
@@ -6,7 +6,6 @@ export const MainContainer = styled.div`
   align-items: center;
   background-color: #fffad0;
   height: 100vh;
-  gap: 360px;
 `
 
 export const CooingLogo = styled.div`
@@ -20,8 +19,35 @@ export const HeaderContainer = styled.div`
   justify-content: space-around;
   gap: 60px;
   align-items: center;
+  margin-top: 30px;
 `
 
 export const ButtonContainer = styled.div`
   cursor: pointer;
 `
+
+export const Line = styled.div`
+  width: 380px;
+  height: 1px;
+  background: #fd814a;
+  display: flex;
+`
+
+export const CanvasContainer = styled.div`
+  background-image: url(${(props) => props.backgroundImg});
+  background-position-x: center;
+  background-position-y: 50px;
+  background-size: 400px 400px;
+  background-repeat: no-repeat;
+`
+
+export const SelectedObj = styled.div`
+  width: 90px;
+  height: 90px;
+  background-image: url(${(props) => props.backgroundImg});
+  background-size: cover;
+  transform: translate(-50%, -50%);
+  //overflow: visible;
+`
+
+export const CanvasComponent = styled.canvas``
