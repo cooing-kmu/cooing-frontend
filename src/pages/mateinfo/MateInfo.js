55024811b464d8f1aa693d52b29862c7ad247a7a
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-22 01:46:50 +0900

    feat: 매칭 정보 페이지 UI 구현

--- src/pages/mateinfo/MateInfo.js
+++ src/pages/mateinfo/MateInfo.js
@@ -1,13 +1,22 @@
 import * as style from './Styles'
 import Header from '../../components/header/Header'
+import { ReactComponent as Ic_User } from '../../assets/icons/icon-user.svg'
 
 export default function MateInfo() {
   return (
     <style.MainContainer>
       <style.BackgroundContainer>
         <Header title='MATE 정보' />
-        메이트 정보 페이지 테스트
+        <style.CircleContainer></style.CircleContainer>
+        <style.ImageContainer>
+          <Ic_User />
+        </style.ImageContainer>
       </style.BackgroundContainer>
+      <style.TextContainer>
+        <span>닉네임</span>
+        <span>역할</span>
+      </style.TextContainer>
+      <span>한줄소개</span>
     </style.MainContainer>
   )
 }
