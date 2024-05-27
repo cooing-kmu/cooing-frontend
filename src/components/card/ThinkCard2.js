dd6de32a8472eda7df4a4861e0064968f4e71d1c
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-27 18:20:43 +0900

    feat: 가로형 헤더 렌더링을 위한 추가 작업

--- src/components/card/ThinkCard2.js
+++ src/components/card/ThinkCard2.js
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 
 const MainContainer = styled.div`
+  justify-content: center;
+
   gap: 16px;
-  margin-top: 80px;
+  ${({ layout }) => (layout === 0 ? 'margin-top: 80px;' : 'margin-top: -50px;')}
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -13,17 +15,18 @@ const MainContainer = styled.div`
     display: none;
   }
   overflow: auto;
-  margin-bottom: 20px;
+  ${({ layout }) =>
+    layout === 0 ? 'margin-bottom: 20px;' : 'margin-bottom: 0px;'}
 `
 
 const SubContainer = styled.div`
-  margin-top: 30px;
+  ${({ layout }) => (layout === 0 ? 'margin-top: 30px;' : 'margin-top: 0px;')}
   gap: 24px;
   display: grid;
   ${({ layout }) =>
     layout === 0
       ? 'grid-template-columns: repeat(2, 1fr);'
-      : 'grid-template-columns: repeat(16, 1fr);'}
+      : 'grid-template-columns: repeat(8, 1fr);'}
   font-weight: 200;
   overflow-x: auto; /* 가로 스크롤 추가 */
   max-width: 480px; /* 최대 너비 설정 */
@@ -83,11 +86,11 @@ const ThinkCard2 = ({ layout, interestList, buttonName }) => {
           </ItemContainer>
         ))}
       </SubContainer>
-      <div>
-        {buttonName === '수정' ? ( // 정보 수정
+      {buttonName && (
+        <div>
           <Button onClick={() => navigate('/think-edit')}>{buttonName}</Button>
-        ) : null}
-      </div>
+        </div>
+      )}
     </MainContainer>
   )
 }
