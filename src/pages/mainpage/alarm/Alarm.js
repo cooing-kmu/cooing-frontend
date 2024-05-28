4fc0636e7de49e9c7b567d176ef2e6eb1ad88837
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-05-28 21:58:01 +0900

    feat: 알림 페이지 UI 구현

--- src/pages/mainpage/alarm/Alarm.js
+++ src/pages/mainpage/alarm/Alarm.js
@@ -1,10 +1,46 @@
-import * as style from './Styles'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../../../components/header/Header'
+import axios from 'axios'
+import * as style from './Styles'
 
 export default function Alarm() {
+  const navigate = useNavigate()
+  const [boardData, setBoardData] = useState([])
+
   return (
-    <style.MainContainer>
-      <Header title={'알림'} />
-    </style.MainContainer>
+    <style.Div>
+      <Header title='알림' />
+
+      <style.MainContainer>
+        {/*{boardData.length > 0 &&*/}
+        {/*  boardData.map((item, index) => (*/}
+        {/*    <div*/}
+        {/*      key={'리워드 지급'}*/}
+        {/*      // onClick={() => handleBoardClick(item.boardId)}*/}
+        {/*    >*/}
+        {/*      <style.ContentsContainer key={'리워드 아이디'}>*/}
+        {/*        <style.TitleSummary>*/}
+        {/*          <style.Title>테스트</style.Title>*/}
+        {/*          <style.Summary>테스트</style.Summary>*/}
+        {/*        </style.TitleSummary>*/}
+        {/*        <style.Time>테스트</style.Time>*/}
+        {/*      </style.ContentsContainer>*/}
+        {/*    </div>*/}
+        {/*  ))}*/}
+        <div
+          key={'리워드 지급'}
+          // onClick={() => handleBoardClick(item.boardId)}
+        >
+          <style.ContentsContainer key={'리워드 아이디'}>
+            <style.TitleSummary>
+              <style.Title>리워드 이름</style.Title>
+              <style.Summary>리워드 내용</style.Summary>
+            </style.TitleSummary>
+            <style.Time>리워드 지급 시간</style.Time>
+          </style.ContentsContainer>
+        </div>
+      </style.MainContainer>
+    </style.Div>
   )
 }
--- src/pages/mainpage/alarm/Styles.js
+++ src/pages/mainpage/alarm/Styles.js
@@ -1,9 +1,49 @@
 import styled from 'styled-components'
 
-export const MainContainer = styled.div`
+export const Div = styled.div`
   display: flex;
   flex-direction: column;
+`
+
+export const MainContainer = styled.div`
+  width: 370px;
+  height: 750px;
+  margin: 80px auto 0;
+  overflow: auto;
+  flex-direction: column;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+export const ContentsContainer = styled.div`
+  height: 80px;
+  width: 100%;
+  display: flex;
   align-items: center;
-  background-color: #fff;
-  height: 100vh;
+  justify-content: start;
+  border-bottom: #a6a6a6 solid 1px;
+  cursor: pointer;
+`
+
+export const TitleSummary = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 310px;
+  gap: 10px;
+`
+
+export const Title = styled.div`
+  font-size: 12px;
+  font-weight: bold;
+`
+
+export const Summary = styled.div`
+  font-size: 12px;
+=`
+
+export const Time = styled.div`
+  display: flex;
+  font-size: 10px;
+  color: #a6a6a6;
 `
