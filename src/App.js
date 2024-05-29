8d9bea1a75615ce1c6946f53bfd9da8166c4bcf9
suwith <suwith@kookmin.ac.kr>
2024-05-29 14:00:48 +0900

    merge: develop 브랜치와 병합

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import { ThemeProvider } from 'styled-components' // 1. Global color를 사용하기 위한 import
-import React, { Component } from 'react'
 import { RecoilRoot } from 'recoil'
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
-import ChattingList from './pages/chatting/ChattingList'
+import Footer from './components/footer/Footer'
+import Chatting from './pages/chatting/Chatting'
 
 function App() {
   return (
@@ -15,10 +15,11 @@ function App() {
       <div id='App'>
         <CookiesProvider>
           <BrowserRouter>
+            <Footer />
             <Routes>
               <Route path='/sign-up' element={<SignUp />}></Route>
               <Route path='/sign-in' element={<SignIn />}></Route>
-              <Route path='/chatting-list' element={<ChattingList />}></Route>
+              <Route path='/chatting' element={<Chatting />}></Route>
             </Routes>
           </BrowserRouter>
         </CookiesProvider>
---
+++ src/components/footer/Styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  margin-bottom: 50px;
+  align-items: center;
+  display: flex;
+  width: 375px;
+  height: 91px;
+  justify-content: space-evenly;
+  background-color: #fff;
+  border-radius: 50px;
+  box-shadow: 0 -10px 10px 0 rgba(0, 0, 0, 0.1);
+`
+
+export const ButtonContainer = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 74px;
+  height: 74px;
+  border-radius: 100px;
+  background: linear-gradient(180deg, #fd814a 0%, #fc5c4c 100%);
+`
+
+export const IconContainer = styled.div`
+  cursor: pointer;
+  background-color: transparent;
+`
