676be6922e56ec0b685de76a5032e307a6214348
suwith <suwith@kookmin.ac.kr>
2024-06-02 04:46:36 +0900

    feat: 채팅방에서의 상대방 메세지 UI 테스트

--- src/pages/chatting/Messages.js
+++ src/pages/chatting/Messages.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import Message from './Message'
+import user1 from '../../assets/images/user1.png'
+import user2 from '../../assets/images/user2.png'
 
 const Messages = () => {
   return (
     <div className='messages'>
       <Message />
-      <Message />
+      <div className='message'>
+        <div className='message-user '>
+          <img src={user2} alt='user' />
+        </div>
+        <div className='messeage-content'>
+          <div className='message-content-text'>안녕</div>
+          <span>14:33</span>
+        </div>
+      </div>
+
+      <div className='message owner'>
+        <div className='message-user '>
+          <img src={user1} alt='user' />
+        </div>
+        <div className='messeage-content owner'>
+          <div className='message-content-text owner'>Hello</div>
+          <span>14:33</span>
+        </div>
+      </div>
       <Message />
       <Message />
       <Message />
