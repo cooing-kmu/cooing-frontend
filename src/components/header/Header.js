7ecf6902c83dd9724c7ee7b1d9cce163836e9728
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-06-02 23:04:46 +0900

    feat: 헤더 및 푸터 변경사항 반영

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import * as style from './Styles'
 import { ReactComponent as Ic_ArrowLeftBk } from '../../assets/icons/icon-arrow-l-bk.svg'
 import { ReactComponent as Ic_ArrowLeftWh } from '../../assets/icons/icon-arrow-l-wh.svg'
-import { ReactComponent as Ic_Pencil } from '../../assets/icons/icon-pencil.svg'
 import { ReactComponent as Ic_Interest } from '../../assets/icons/icon-interest.svg'
 import { ReactComponent as Ic_Think } from '../../assets/icons/icon-think.svg'
 import { ReactComponent as Ic_Search } from '../../assets/icons/icon-search.svg'
@@ -24,6 +22,8 @@ export default function Header(props) {
     '자립 체크리스트',
     '프로필 변경',
     '매칭 정보 변경',
+    '지원사업 스크랩 목록',
+    '채용공고 스크랩 목록',
   ]
   const ThreeHeaderNameList = [
     '자유 게시판', // 돋보기
@@ -39,11 +39,10 @@ export default function Header(props) {
   ]
 
   return (
-    // MATE 정보, 프로필 변경은 흰색 헤더 사용
     <>
       {props.title === TwoHeaderNameList[7] ||
       props.title === TwoHeaderNameList[10] ? (
-        <style.HeaderContainer IsBlack={false}>
+        <style.HeaderContainer style={{ color: 'white' }}>
           <style.ButtonContainer>
             <Ic_ArrowLeftWh onClick={() => navigate(-1)} />
           </style.ButtonContainer>
@@ -54,7 +53,7 @@ export default function Header(props) {
         </style.HeaderContainer>
       ) : (
         // 나머지는 검은색 헤더 사용
-        <style.HeaderContainer IsBlack={true}>
+        <style.HeaderContainer style={{ color: 'black' }}>
           <style.ButtonContainer>
             {props.title === ThreeHeaderNameList[8] ||
             props.title === ThreeHeaderNameList[9] ||
