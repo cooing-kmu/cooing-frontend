0f2197ed960832958bd3c54f9d894ca9849ef977
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-06-02 23:21:00 +0900

    feat: 찬우오빠 관련 코드 삭제

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Profile from './pages/profile/Profile'
 import CheckList from './pages/checklist/CheckList'
 import InterestInfo from './pages/interestinfo/InterestInfo'
 import Announcement from './pages/announcement/Announcement'
-import Community from './pages/community/Community'
 import Chatting from './pages/chatting/Chatting'
 import MainPage from './pages/mainpage/MainPage'
 import ThinkInfo from './pages/thinkinfo/ThinkInfo'
@@ -18,9 +17,6 @@ import InterestEdit from './pages/interestedit/InterestEdit'
 import ThinkEdit from './pages/thinkedit/ThinkEdit'
 import Alarm from './pages/mainpage/alarm/Alarm'
 import MateInfo from './pages/mainpage/mateinfo/MateInfo'
-import FreeBoard from './pages/community/freeboard/FreeBoard'
-import FreeBoardWrite from './pages/community/freeboard/FreeBoardWrite'
-import FreeBoardPost from './pages/community/freeboard/FreeBoardPost'
 
 function App() {
   return (
@@ -39,7 +35,6 @@ function App() {
               <Route path='/sign-up' element={<SignUp />}></Route>
               <Route path='/sign-in' element={<SignIn />}></Route>
               <Route path='/announcement' element={<Announcement />}></Route>
-              <Route path='/community' element={<Community />}></Route>
               <Route path='/main-page' element={<MainPage />}></Route>
               <Route path='/chatting' element={<Chatting />}></Route>
               <Route path='/interest-edit' element={<InterestEdit />}></Route>
@@ -47,15 +42,6 @@ function App() {
               <Route path='/think-edit' element={<ThinkEdit />}></Route>
               <Route path='/alarm' element={<Alarm />}></Route>
               <Route path='/mate-info' element={<MateInfo />}></Route>
-              <Route path='/free-board' element={<FreeBoard />}></Route>
-              <Route
-                path='/free-board-write'
-                element={<FreeBoardWrite />}
-              ></Route>
-              <Route
-                path='/free-board-post/:boardId'
-                element={<FreeBoardPost />}
-              />
             </Routes>
           </BrowserRouter>
         </CookiesProvider>
