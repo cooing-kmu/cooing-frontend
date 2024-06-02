6016d2faa7b2533d1bdcded9061be8a0fcf0f348
suwith <suwith@kookmin.ac.kr>
2024-06-03 04:28:25 +0900

    delete: 불필요한 파일 삭제

--- src/pages/chatting/test/UserListBox.js
+++
@@ -1,6 +0,0 @@
-const UserListBox = (props) => {
-  console.log('===============\n', props.userListTsx)
-  return <div className='userListBoxContainer'>{props.userListTsx}</div>
-}
-
-export default UserListBox
