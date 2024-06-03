9744fdc55da2a87b76f1211ccb3efd7dd023671f
suwith <suwith@kookmin.ac.kr>
2024-06-03 22:45:59 +0900

    feat: 추천친구 API 연결 완료

--- src/pages/chatting/Recommend.js
+++ src/pages/chatting/Recommend.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UserData from '../../data/UserData'
 import RecommendData from '../../data/RecommendData'
 import theme from '../../Theme'
 import './Chatting.css'
 import { useRecoilState } from 'recoil'
-import { userState } from '../../utils/userAtom'
+import { tokenState, userState } from '../../utils/userAtom'
 import concernIcon from '../../assets/icons/icon-concern.svg'
 import interestIcon from '../../assets/icons/icon-heart.svg'
+import { DOMAIN_NAME } from '../../App'
 export default function Recommend() {
+  const [token, setToken] = useRecoilState(tokenState)
   const [user, setUser] = useRecoilState(userState)
 
   const [currentRecommend, setCurrentRecommend] = useState(true)
@@ -19,8 +21,36 @@ export default function Recommend() {
           ? '님의 고민을 해결해 줄 그레텔'
           : '님이 도움을 줄 수 있는 헨젤'
       }님과 대화를 나누어 보세요!`
+  const [recommendUserList, setRecommendUserList] = useState([])
 
   console.log(user)
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          `${DOMAIN_NAME}/user/suggestions?userId=${user.id}`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        )
+        const data = await response.json()
+        const users = data.body
+
+        console.log(data)
+        console.log(users)
+
+        setRecommendUserList(users)
+
+        if (!users) return
+      } catch (error) {
+        console.error('Failed to fetch users:', error)
+      }
+    }
+
+    fetchUsers()
+  }, [token, currentRecommend])
 
   return (
     <div className='recommend'>
@@ -36,15 +66,18 @@ export default function Recommend() {
         </div>
       </div>
       <div className='recommend-list'>
-        {RecommendData.map((item, index) => (
+        {recommendUserList.map((item, index) => (
           <div key={index} className='recommend-list-user'>
             <img
-              src={item.img}
-              alt={item.name}
+              src={item.profileImageUrl}
+              alt={item.username}
               // 이미지에 상대적으로 더 많은 높이 할당
             />
-            <div style={{ textAlign: 'center', height: '20%' }}>
-              {item.name}
+            <div
+              className='recommend-list-user-text'
+              style={{ textAlign: 'center', height: '20%' }}
+            >
+              {item.username}
             </div>
             {/* 텍스트 영역에도 고정된 높이를 할당하여, 전체 div 높이 내에서 정확한 비율 유지 */}
           </div>
