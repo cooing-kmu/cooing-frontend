99768036b7ef3f0d79bb1b0778135c41d0d25c31
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-06-03 19:37:16 +0900

    feat: 메이트 정보 페이지 API 연동

--- src/pages/mainpage/mateinfo/MateInfo.js
+++ src/pages/mainpage/mateinfo/MateInfo.js
@@ -3,11 +3,46 @@ import Header from '../../../components/header/Header'
 import { ReactComponent as Ic_User } from '../../../assets/icons/icon-user.svg'
 import InterestCard2 from '../../../components/card/InterestCard2'
 import ThinkCard2 from '../../../components/card/ThinkCard2'
-import { TextContainer } from './Styles'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { DOMAIN_NAME } from '../../../App'
 
 export default function MateInfo() {
-  const MateInterestKeyword = [0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0]
-  const MateThinkKeyword = [0, 1, 0, 0, 1, 0, 1, 1]
+  const [user, setUser] = useState(null)
+  const [nickname, setNickname] = useState('')
+  const [role, setRole] = useState('')
+  const [profileMessage, setProfileMessage] = useState('')
+  const [profileImageUrl, setProfileImageUrl] = useState(null)
+  const [interestKeywords, setInterestKeywords] = useState([])
+  const [concernKeywords, setConcernKeywords] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tokenResponse = await axios.get(`${DOMAIN_NAME}/test-user`)
+        const token = tokenResponse.data.body
+
+        const userResponse = await axios.get(`${DOMAIN_NAME}/user/mate`, {
+          headers: {
+            Authorization: token,
+          },
+        })
+
+        const _user = userResponse.data.body
+        setUser({ ..._user, token })
+        setNickname(_user.name)
+        setRole(_user.role)
+        setProfileMessage(_user.profileMessage)
+        setProfileImageUrl(_user.profileImageUrl)
+        setInterestKeywords(_user.interestKeyword)
+        setConcernKeywords(_user.concernKeyword)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <style.MainContainer>
@@ -15,26 +50,26 @@ export default function MateInfo() {
         <Header title='MATE 정보' />
         <style.CircleContainer></style.CircleContainer>
         <style.ImageContainer>
-          <Ic_User />
+          {profileImageUrl ? (
+            <style.ProfileImage src={profileImageUrl} alt='Profile' />
+          ) : (
+            <Ic_User />
+          )}
         </style.ImageContainer>
       </style.BackgroundContainer>
       <style.InfoContainer>
-        <span>닉네임</span>
-        <span>역할</span>
+        <span>{nickname}</span>
+        <span>{role}</span>
       </style.InfoContainer>
-      <style.BioContainer>한줄소개</style.BioContainer>
+      <style.BioContainer>{profileMessage}</style.BioContainer>
       <style.TextContainer>관심 키워드</style.TextContainer>
       <InterestCard2
         layout={1}
-        interestList={MateInterestKeyword}
+        interestList={interestKeywords}
         buttonName={null}
       />
       <style.TextContainer>고민 키워드</style.TextContainer>
-      <ThinkCard2
-        layout={1}
-        interestList={MateThinkKeyword}
-        buttonName={null}
-      />
+      <ThinkCard2 layout={1} interestList={concernKeywords} buttonName={null} />
       <span
         style={{ marginBottom: '20px', color: '#a6a6a6', fontSize: '14px' }}
       >
