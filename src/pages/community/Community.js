46a7391e4d0291f7bc3beb90594fce09e56a0d39
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-06-05 02:39:27 +0900

    feat: 게시판 페이지 병합

--- src/pages/community/Community.js
+++ src/pages/community/Community.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import freeBoard from '../../assets/freeBoard.svg'
-import pencil from '../../assets/pencil.svg'
-import message from '../../assets/message.svg'
-import star from '../../assets/star.svg'
-import volunteer from '../../assets/volunteer.svg'
-import club from '../../assets/club.svg'
-import study from '../../assets/study.svg'
+import freeBoard from '../../assets/icons/icon-freeboard.svg'
+import pencil from '../../assets/icons/icon-pencil-o.svg'
+import message from '../../assets/icons/icon-comment-o.svg'
+import star from '../../assets/icons/icon-star-o.svg'
+import volunteer from '../../assets/icons/icon-volunteer.svg'
+import club from '../../assets/icons/icon-club.svg'
+import study from '../../assets/icons/icon-study.svg'
 import { useNavigate } from 'react-router-dom'
 import Footer from '../../components/footer/Footer'
 
@@ -16,6 +16,7 @@ const Container = styled.div`
   background-color: #fffad0;
   flex-direction: column;
   height: 100vh; /* 화면 전체 높이 설정 */
+  width: 100%;
 `
 
 const MainContainer = styled.div`
@@ -26,7 +27,7 @@ const MainContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  box-shadow: 0px 4px 4px 0 rgba(0,0,0,0.25);
+  box-shadow: 0px 4px 4px 0 rgba(0, 0, 0, 0.25);
 `
 
 const SubLineContainer = styled.div`
@@ -82,52 +83,48 @@ export default function Community() {
     <Container>
       <Title>헨젤과 그레텔의 속닥속닥</Title>
       <MainContainer>
-
         <SubLineContainer onClick={handleFreeBoardClick}>
-            <img src={freeBoard} alt={"img"}/>
-            자유 게시판
+          <img src={freeBoard} alt={'img'} />
+          자유 게시판
         </SubLineContainer>
-        <Line/>
+        <Line />
 
         <SubLineContainer onClick={handleMyWroteClick}>
-          <img src={pencil} alt={"img"}/>
+          <img src={pencil} alt={'img'} />
           내가 쓴 글
         </SubLineContainer>
-        <Line/>
+        <Line />
 
         <SubLineContainer onClick={handleCommentClick}>
-          <img src={message} alt={"img"}/>
+          <img src={message} alt={'img'} />
           댓글 단 글
         </SubLineContainer>
-        <Line/>
+        <Line />
 
         <SubLineContainer onClick={handleScrapClick}>
-          <img src={star} alt={"img"}/>
+          <img src={star} alt={'img'} />
           스크랩한 글
         </SubLineContainer>
-
       </MainContainer>
 
       <Title>헨젤 X 대학생 그레텔</Title>
       <MainContainer>
-
         <SubLineContainer onClick={handleVolunteerClick}>
-          <img src={volunteer} alt={"img"}/>
+          <img src={volunteer} alt={'img'} />
           봉사활동
         </SubLineContainer>
-        <Line/>
+        <Line />
 
         <SubLineContainer onClick={handleClubClick}>
-          <img src={club} alt={"img"}/>
+          <img src={club} alt={'img'} />
           동아리 및 소모임
         </SubLineContainer>
-        <Line/>
+        <Line />
 
         <SubLineContainer onClick={handleStudyClick}>
-          <img src={study} alt={"img"}/>
+          <img src={study} alt={'img'} />
           스터디
         </SubLineContainer>
-
       </MainContainer>
 
       <Footer />
