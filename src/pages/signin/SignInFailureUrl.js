0cd5807aa37754fa73c858dfaeef7a4ad6444ed4
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-06-05 02:35:23 +0900

    feat: 로그인 페이지 병합

--- src/pages/signin/SignInFailureUrl.js
+++ src/pages/signin/SignInFailureUrl.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const MainContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color:#FFFAD0;
+  background-color: #fffad0;
   height: 100vh;
   font-size: 40px;
-`;
-
+  width: 100%;
+`
 
 export default function SignInFailureUrl() {
-    return (
-        <MainContainer>
-            로그인 실패
-        </MainContainer>
-    );
+  return <MainContainer>로그인 실패</MainContainer>
 }
